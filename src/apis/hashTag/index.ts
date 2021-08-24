896a1b41b69db221fa8aa602a188a8306c184022
seojonghak <unqocn@gmail.com>
2021-08-24 22:03:16 +0900

    ing: 해시태그 추가, 임시저장기능 추가

---
+++ src/apis/hashTag/index.ts
@@ -0,0 +1,31 @@
+const MOCK_TAGS = [
+  "fontfamily(1)",
+  "fontsize(1)",
+  "bold(2)",
+  "underline(1)",
+  "italic(11)",
+  "strikethrough(1)",
+  "fontColor(1)",
+  "fontBackgroundColor(1)",
+  "alignment(1)",
+  "horizontalLine(1)",
+  "numberedList(1)",
+  "lineHeight(1)",
+  "blockQuote(1)",
+  "insertTable(1)",
+  "link(1)",
+  "uploadImage(1)",
+  "mediaEmbed(1)",
+  "videoUpload(1)",
+    "가나다(8)"
+];
+
+export const checkHashKeyword = async (keyword:string):Promise<string[]> =>{
+    return new Promise((resolve, reject) => {
+        setTimeout(()=>{
+            resolve(MOCK_TAGS.filter((tag)=>{
+                return tag.includes(keyword)
+            }))
+        },400)
+    })
+}
--- src/components/CustomEditor/index.tsx
+++ src/components/CustomEditor/index.tsx
@@ -1,15 +1,52 @@
-import React, { useRef } from "react";
+import React, { ChangeEvent, useRef, useState } from "react";
 // @ts-ignore
 import { CKEditor } from "@ckeditor/ckeditor5-react";
 import { EditorUploadAdapterPlugin } from "./plugins/EditorUploadAdapterPlugin";
 import { MOCK_DATA } from "./mock/data";
 import "./font.css";
+import "./style.css";
+import { checkHashKeyword } from "../../apis/hashTag";
 const ClassicEditor = require("../../ckeditor/build/ckeditor");
 
-export interface Props {}
+export interface Props {
+  onChange?: (data: any) => any;
+}
 
-const CustomEditor: React.FC<Props> = (props: Props) => {
+const CustomEditor: React.FC<Props> = ({ onChange }: Props) => {
   const editorRef = useRef(null);
+  const [data, setData] = useState(MOCK_DATA);
+  const [saveCount, setSaveCount] = useState<number>(
+    () => Number(window.localStorage.getItem("cckEditorSaveCount")) || 0
+  );
+  const [autoList, setAutoList] = useState<string[]>([])
+
+  const handleSave = () => {
+    setSaveCount((prev) => prev + 1);
+    window.localStorage.setItem("cckEditorSaveCount", `${saveCount + 1}`);
+    window.localStorage.setItem("cckEditor", data.toString());
+    alert("임시 저장 되었습니다.");
+  };
+
+  const getSaveData = () => {
+    setData(window.localStorage.getItem("cckEditor") || "");
+  };
+
+  const handleTagChange = async ({
+    target: { value },
+  }: ChangeEvent<HTMLInputElement>) => {
+    console.log(value)
+    if (value){
+      try {
+        const autoList = await checkHashKeyword(value)
+        setAutoList(autoList);
+      } catch (e) {
+        console.error(e);
+      }
+    }else {
+      setAutoList([])
+    }
+  };
+  console.log(autoList)
 
   const editorConfig = {
     extraPlugins: [EditorUploadAdapterPlugin],
@@ -62,17 +99,33 @@ const CustomEditor: React.FC<Props> = (props: Props) => {
   };
 
   return (
-    <CKEditor
-      ref={editorRef}
-      editor={ClassicEditor}
-      config={editorConfig}
-      data={MOCK_DATA}
-      onChange={(event: any, editor: { getData: () => any }) => {
-        const data = editor.getData();
-        console.log(data);
-        // if (onChange) onChange(data);
-      }}
-    />
+    <div>
+      <CKEditor
+        ref={editorRef}
+        editor={ClassicEditor}
+        config={editorConfig}
+        data={data}
+        onChange={(event: any, editor: { getData: () => any }) => {
+          const data = editor.getData();
+          setData(data);
+          console.log(data);
+          if (onChange) onChange(data);
+        }}
+      />
+      <div style={{ position: "relative" }}>
+        <p>태그 자동완성 테스트</p>
+        <input onChange={handleTagChange} />
+        <ul className={"autoTagList"}>
+          {autoList.map((keyword) => {
+            return <li key={keyword}>{keyword}</li>;
+          })}
+        </ul>
+      </div>
+      <div>
+        <button onClick={handleSave}>임시저장({saveCount})</button>
+        <button onClick={getSaveData}>불러오기</button>
+      </div>
+    </div>
   );
 };
 
