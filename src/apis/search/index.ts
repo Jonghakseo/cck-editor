eb609d370611a95a5956e7fe3c46f4542b4a88ce
seojonghak <unqocn@gmail.com>
2021-08-29 16:53:11 +0900

    ing: 지도작업

--- src/apis/search/index.ts
+++ src/apis/search/index.ts
@@ -1,18 +1,33 @@
-import {Method, request} from "../../network/Request";
+import { Method, request } from "../../network/Request";
 
 // const realUrl = "https://naveropenapi.apigw.ntruss.com/map-geocode/v2/geocode"
-const lambdaUrl = "https://pvcp4p1ii9.execute-api.ap-northeast-2.amazonaws.com/default/searchNaverKeyword"
+const lambdaUrl =
+  "https://pvcp4p1ii9.execute-api.ap-northeast-2.amazonaws.com/default/searchNaverKeyword";
 
-export const keywordSearch = async (keyword:string) =>  {
-    const {data } =await request({
-        method: Method.GET,
-        baseUrl: lambdaUrl,
-        queryParams:{
-            key:keyword
-        }
-    });
-
-    return JSON.parse(data)
+export interface SearchResultItem {
+  address: string;
+  category: string;
+  description: string;
+  link: string;
+  mapx: number;
+  mapy: number;
+  roadAddress: string;
+  telephone: string;
+  title:string
+}
+export interface SearchResult {
+  display: number;
+  items: SearchResultItem[];
 }
 
+export const keywordSearch = async (keyword: string) => {
+  const { data } = await request({
+    method: Method.GET,
+    baseUrl: lambdaUrl,
+    queryParams: {
+      key: keyword,
+    },
+  });
 
+  return JSON.parse(data) as SearchResult;
+};
--- src/pages/map.tsx
+++ src/pages/map.tsx
@@ -1,36 +1,84 @@
-import React, { useEffect } from "react";
-import {keywordSearch} from "../apis/search";
+import React, { useEffect, useRef, useState } from "react";
+import { keywordSearch, SearchResultItem } from "../apis/search";
 
 declare const naver: any;
 
 export interface Props {}
 
 const MapPage: React.FC<Props> = (props: Props) => {
+  const mapRef = useRef<any>(null);
+  const map = mapRef.current;
+  const inputRef = useRef<null | HTMLInputElement>(null);
+  const [searchResults, setSearchResults] = useState<SearchResultItem[]>([]);
 
-  const handleSearch = async (key:string) =>{
-    try {
-      const res = await keywordSearch(key)
-      console.log(res)
-    }catch (e) {
-      console.error(e)
+  const moveMap = (x: number, y: number) => {
+    const point = new naver.maps.Point(x, y);
+    const latLng = naver.maps.TransCoord.fromTM128ToLatLng(point);
+    map.setCenter(latLng); // 중심 좌표 이동
+    map.setZoom(14); // 줌 레벨 변경
+
+    var marker = new naver.maps.Marker({
+      position: latLng,
+      map: map,
+    });
+  };
+
+  const handleSearch = async () => {
+    const input = inputRef.current;
+    if (input !== null) {
+      const key = input?.value;
+      try {
+        const { items } = await keywordSearch(key);
+        console.log(items);
+        setSearchResults(items);
+      } catch (e) {
+        console.error(e);
+      }
     }
-  }
+  };
 
   useEffect(() => {
     setTimeout(() => {
       var mapOptions = {
-        center: new naver.maps.LatLng(37.3595704, 127.105399),
+        center: new naver.maps.LatLng(37.3895704, 127.105399),
+        mapDataControl: true,
         zoom: 10,
       };
-      var map = new naver.maps.Map("map", mapOptions);
-      handleSearch("금돼지식당")
+      mapRef.current = new naver.maps.Map("map", mapOptions);
     }, 100);
+
+    return () => {
+      mapRef.current.destroy();
+    };
   }, []);
   return (
-    <>
-      <div id={"map"} style={{ width: "400px", height: "400px" }} />
-      map
-    </>
+    <article>
+      <div className={"map_header"}>
+        <input
+          ref={inputRef}
+          onKeyDown={(e) => {
+            if (e.key === "Enter") handleSearch();
+          }}
+        />
+        <button onClick={handleSearch}>검색</button>
+      </div>
+      <div className={"map_contents"} style={{ display: "flex" }}>
+        <div className={"map_search_result"}>
+          {searchResults?.map((item, index) => {
+            return (
+              <div
+                key={index.toString()}
+                onClick={() => moveMap(item.mapx, item.mapy)}
+              >
+                <h3>{item.title}</h3>
+                <p>{item.roadAddress}</p>
+              </div>
+            );
+          })}
+        </div>
+        <div id={"map"} style={{ width: "400px", height: "400px" }} />
+      </div>
+    </article>
   );
 };
 
